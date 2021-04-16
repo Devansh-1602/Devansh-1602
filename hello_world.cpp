020dd50f00279953af41746e7d6d789b873e6026
Devil <devansh.mehra24@hmail.com>
2021-04-16 15:02:10 +0530

    initial commit

---
+++ hello_world.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int  main(){
+  int a=1234;
+  cout<<"size of int"<<sizeof(a)<<endl;
+  char b;
+   cout<<"size of char"<<sizeof(b)<<endl;
+  return 0;
+}
